4afb56101ea977115a342c65a39af645b7f4f617
gamadigdojo <gamadigdojo@gmail.com>
2023-03-19 17:10:42 +0700

    homepage done

--- Homepage/homepage.js
+++ Homepage/homepage.js
@@ -10,24 +10,6 @@ window.onscroll = () => {
     navbar.classList.remove('active')
 }
 
-const products = document.querySelectorAll('.box');
-let currentProductIndex = 0;
 
-function showNextProduct() {
-  // remove zoom-in class from current product and add zoom-out class
-  products[currentProductIndex].classList.remove('zoom-in');
-  products[currentProductIndex].classList.add('zoom-out');
 
-  // update index to show next product
-  currentProductIndex++;
-  if (currentProductIndex >= products.length) {
-    currentProductIndex = 0;
-  }
 
-  // remove zoom-out class from next product and add zoom-in class
-  products[currentProductIndex].classList.remove('zoom-out');
-  products[currentProductIndex].classList.add('zoom-in');
-}
-
-const nextBtn = document.querySelector('.next-btn');
-nextBtn.addEventListener('click', showNextProduct);
